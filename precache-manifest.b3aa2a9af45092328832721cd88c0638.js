1e5bf103a23fbf1df50133dacd013ac4e1dbf483
SamirAhma <30921483+SamirAhma@users.noreply.github.com>
2019-12-18 18:01:35 +0800

    Add files via upload

---
+++ precache-manifest.b3aa2a9af45092328832721cd88c0638.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cc4d5511145d8d69fa5060ad4a52591",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1050a541dd9da92911f2",
+    "url": "/static/css/main.d6fa7445.chunk.css"
+  },
+  {
+    "revision": "0ff939334e2b3911df21",
+    "url": "/static/js/2.9b4447ab.chunk.js"
+  },
+  {
+    "revision": "1050a541dd9da92911f2",
+    "url": "/static/js/main.33dd3732.chunk.js"
+  },
+  {
+    "revision": "96b9e444592fbee54994",
+    "url": "/static/js/runtime~main.26283e15.js"
+  },
+  {
+    "revision": "84753208e3ace6c36a3a29c731983a69",
+    "url": "/static/media/slide_one.84753208.jpg"
+  },
+  {
+    "revision": "9aa09a50b7eb84e96d522c10dfaa78e5",
+    "url": "/static/media/slide_three.9aa09a50.jpg"
+  },
+  {
+    "revision": "3a3ffd3696fea8999087bbdcf97d69d2",
+    "url": "/static/media/slide_two.3a3ffd36.jpg"
+  }
+]);
